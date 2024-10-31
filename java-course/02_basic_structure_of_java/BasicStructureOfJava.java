aa5b3e8e768cc747200cd7abf20d5be2db58f823
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-10-30 22:47:36 -0400

    Add IntroToJava and BasicStructureOfJava classes to Java course

---
+++ java-course/02_basic_structure_of_java/BasicStructureOfJava.java
@@ -0,0 +1,11 @@
+public class BasicStructureOfJava {
+   // function --> only certain work
+   // class --> combination of diff function
+   // object --> instance of class
+   public static void main(String[] args) {
+      // public --> access modifier; Since the main method needs to be called by the (JRE), it must be public
+      // static --> main method belongs to the class itself rather than an instance of the class
+      // void --> this function doesn't return anything
+      // main(String[] args) --> entry point; array of String objects that stores CLI
+   }
+}
