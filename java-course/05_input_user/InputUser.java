65e324417473d20404eecd5b686cc3d903229fa0
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-10-31 21:44:26 -0400

    Refactor InputUser class in Java course for user input

--- java-course/05_input_user/InputUser.java
+++ java-course/05_input_user/InputUser.java
@@ -11,9 +11,20 @@ public class InputUser {
       int sum = num1 + num2;
       System.out.println(sum);
       
-      // returns true if isNum is int only
-      boolean isNum = sc.hasNextInt();
-      System.out.println(isNum);
+      // // returns true if isNum is int only
+      // boolean isNum = sc.hasNextInt();
+      // System.out.println(isNum);
+
+      // String
+      String name2 = sc.nextLine();
+      System.out.println(name2);
+      String name = sc.next();
+      System.out.println(name);
+
+      // next()
+      /* it reads a single word (up to the first space) and leaves the newline character (from pressing Enter) in the input buffer. As a result, when you then call sc.nextLine() to read name2, it captures that leftover newline character instead of waiting for new input. */
+      // use nextLine always
+
 
    }
 }
