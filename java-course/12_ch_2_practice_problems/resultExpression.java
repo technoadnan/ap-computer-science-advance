5be424a23e99a2ddd988b5953f4fdcda1f151d2e
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-08 22:47:33 -0500

    Add resultExpression class to Java course for demonstrating float arithmetic

---
+++ java-course/12_ch_2_practice_problems/resultExpression.java
@@ -0,0 +1,11 @@
+public class resultExpression {
+
+   public static void main(String[] args) {
+      float a = 7/4 * 9/2;
+      // 7/4 = 1
+      // 9/2 = 4 
+      // 1 * 4 = 4
+
+      System.out.println(a);
+   }
+}
