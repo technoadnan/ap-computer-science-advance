2aeac1a41b3daace189a5356aa88187022cfd779
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-09 22:33:12 -0500

    Add guessTheNumber class to Java course for guessing a number

---
+++ java-course/12_ch_2_practice_problems/guessTheNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class guessTheNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = 45;
+        System.out.println("Enter a number: ");
+        int guess = sc.nextInt();
+        if (guess > number) {
+            System.out.println("Your number is too high");
+        }
+        else if (guess < number) {
+            System.out.println("Your number is too low");
+        }
+        else {
+            System.out.println("Your number is correct");
+        }
+
+
+
+    }
+}
