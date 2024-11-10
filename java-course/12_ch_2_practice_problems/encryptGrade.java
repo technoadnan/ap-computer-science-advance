fe8d2e7561729baa4150479828df64678245d16e
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-09 22:33:03 -0500

    Add encryptGrade class to Java course for encrypting and decrypting grades

---
+++ java-course/12_ch_2_practice_problems/encryptGrade.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class encryptGrade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        char grade = 'A';
+        System.out.println("Enter the grade of the student");
+        char grade = sc.next().charAt(0);
+//        type casting & encrypt
+        grade = (char)(grade + 8);
+//        Decrypt
+        grade = (char)(grade - 8);
+        System.out.printf("The grade of the student is %c", grade);
+        sc.close();
+
+
+
+    }
+}
