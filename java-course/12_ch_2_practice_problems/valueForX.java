56591ae356528e9c60e561fba38693570cb07c81
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-09 22:33:23 -0500

    Add valueForX class to Java course for calculating the value of an expression

---
+++ java-course/12_ch_2_practice_problems/valueForX.java
@@ -0,0 +1,10 @@
+public class valueForX {
+    public static void main(String[] args) {
+//        (7*49/7+35/7)
+//        7 * 49 = 343
+//        343 / 7 = 49
+//        35 / 7 = 5
+//        49 + 5 = 54
+//        Final answer is 54
+    }
+}
