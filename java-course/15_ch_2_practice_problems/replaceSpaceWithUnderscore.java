db7ebcebab395b134be9105b5f85024afd0f38d4
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-12 23:49:36 -0500

    Add replaceSpaceWithUnderscore class to Java course for replacing spaces with underscores in a string

---
+++ java-course/15_ch_2_practice_problems/replaceSpaceWithUnderscore.java
@@ -0,0 +1,7 @@
+public class replaceSpaceWithUnderscore {
+   public static void main(String[] args) {
+      String name = "Adnan Bin Alam";
+      System.out.println(name.replace(" ", "_"));
+
+   }   
+}
