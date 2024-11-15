30aca2c224ae1ec09c9c88947c94d338553082fc
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-14 22:22:24 -0500

    Add realationalLogicalOperators class to Java course for demonstrating relational and logical operators

---
+++ java-course/17_realational_logical_operators/realationalLogicalOperators.java
@@ -0,0 +1,37 @@
+public class realationalLogicalOperators {
+   public static void main(String[] args) {
+      // realational operators 
+      // == => > < <= !=
+      // = is assigments operators
+      
+
+      // Logical operators
+      // && || !=
+
+      //       AND Operator : 
+      // Evaluates to true if both the conditions are true.
+
+      // Y && Y = Y                
+      // Y && N = N
+      // N && Y = N
+      // N && N = N
+
+
+      // OR Operator :
+      // Evaluates to true when at least one of the conditions is true.
+
+      // Y || Y = Y                   
+      // Y || N = Y
+      // N || Y = Y
+      // N || N = N
+      // Convention: # Y – True and N - False
+
+      // NOT Operator :
+      // Negates the given logic (true becomes false and vice-versa)
+
+      // !Y = N                         
+      // !N = Y
+
+      
+   }
+}
