301a7cdfa0a62083e150a16ebe681f26b76b2863
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-14 22:22:04 -0500

    Add ifElseStatements class to Java course for demonstrating if-else statements

---
+++ java-course/16_if_else_statements/ifElseStatements.java
@@ -0,0 +1,12 @@
+public class ifElseStatements {
+   public static void main(String[] args) {
+      int age = 45;
+      if (age > 50) {
+         System.out.println("more than 45");
+      }
+      else{
+         System.out.println("less than 45");
+      }
+
+   }
+}
