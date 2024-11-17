881d86038aeaebfecfc027a9a06f0e332ed93ca9
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-16 22:44:53 -0500

    Add studentPassOrFail class to Java course for checking if a student passes or fails based on their grades

---
+++ java-course/19_ch_4_practice_problems/studentPassOrFail.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class studentPassOrFail {
+
+   public static void main(String[] args) {
+      // sou
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the grade for 1st sub: ");
+      int mark1 = sc.nextInt();
+      System.out.println("Enter the grade for 2nd sub: ");
+      int mark2 = sc.nextInt();
+      System.out.println("Enter the grade for 3rd sub: ");
+      int mark3 = sc.nextInt();
+
+      int avg = (mark1 + mark2 + mark3)/3;
+
+      if (mark1 >= 33 && mark2 >= 33 && mark3 >= 33) {
+         if (avg >= 40) {
+            System.out.println("Pass");
+         }
+      } else {
+         System.out.println("fail");
+      }
+   }
+}
