3a1ddb917bb9d60653bdcf34e7fe40c8a3b66b73
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-19 22:36:30 -0500

    Add WhileLoop class to Java course for demonstrating the use of while loops

---
+++ java-course/21_while_loops/WhileLoop.java
@@ -0,0 +1,14 @@
+public class WhileLoop {
+   public static void main(String[] args) {
+      
+      int number = 56;
+      int i = 0;
+      while (number > i) {
+         System.out.println(i);
+         i++;
+      }
+
+      // if the condition is True its an infitive loop
+      
+   }   
+}
