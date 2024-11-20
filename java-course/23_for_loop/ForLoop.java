ecaf9ebe498a2a4f40fe88f0559aaa9a52cf20dc
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-19 22:36:17 -0500

    Add ForLoop class to Java course for demonstrating the use of for loops

---
+++ java-course/23_for_loop/ForLoop.java
@@ -0,0 +1,18 @@
+public class ForLoop {
+   public static void main(String[] args) {
+      // for (int i = 1; i < 10; i++) {
+      //    System.out.println(i);
+      // }
+      // even number -> 2n
+      // odd number -> (2n + 1)
+
+      // for (int i = 0; i < 5; i++) {
+      //    System.out.println(2*i+1);
+      // }
+
+      // print the first n natural numbers in reverse order
+      for (int i = 10; i > 0; i--) {
+         System.out.println(i);
+      }
+   }   
+}
