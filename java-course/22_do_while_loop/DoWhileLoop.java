fb3b85c0f125ee885b6561235ad1e71cd41ec987
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-19 22:36:23 -0500

    Add DoWhileLoop class to Java course for demonstrating the use of do-while loops

---
+++ java-course/22_do_while_loop/DoWhileLoop.java
@@ -0,0 +1,10 @@
+public class DoWhileLoop {
+   public static void main(String[] args) {
+      int b = 10;
+      do {
+         System.out.println(b);
+         b++;
+      } while (b < 5); // even though the the while condition is false it goes to DO 
+      
+   }
+}
