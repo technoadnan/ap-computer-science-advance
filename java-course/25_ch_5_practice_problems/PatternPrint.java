42fa70a4d9921fb44b9ffcd994c443fde5cabb0d
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-20 23:39:37 -0500

    Add PatternPrint class to Java course for printing a pattern of asterisks

---
+++ java-course/25_ch_5_practice_problems/PatternPrint.java
@@ -0,0 +1,10 @@
+public class PatternPrint {
+   public static void main(String[] args) {
+      for (int i = 4; i > 0; i--) {
+         for (int j = 0; j < i; j++) {
+            System.out.print('*');
+         }
+         System.out.println();
+      }
+   }
+}
