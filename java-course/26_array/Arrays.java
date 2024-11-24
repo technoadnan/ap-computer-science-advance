24e2377c9e53b351671ead778d52e21a2562f0c2
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-23 23:59:19 -0500

    Refactor Arrays class to include examples of array declaration and memory allocation

--- java-course/26_array/Arrays.java
+++ java-course/26_array/Arrays.java
@@ -1,5 +1,17 @@
 package java-course.26_array;
 
 public class Arrays {
+   public static void main(String[] args) {
+      int[] marks; // declaration
+      marks = new int[5]; // mem alloctions
+
+      int[] marks1 = new int[5];// declarations + mem alloctions
+
+      int[] marks2 = {100,200,30}; // declaration + mem alloctions
+
+      // each int takes 4 bytes so 4*size of array 
+   }
+   // declaration
+
    
 } 
