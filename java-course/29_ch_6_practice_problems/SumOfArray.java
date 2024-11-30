4eb60c39e2f4f7bfe2c678211448f70c5ca9ccf7
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-29 23:54:05 -0500

    Add SumOfArray class to Java course for calculating the sum of elements in an array

---
+++ java-course/29_ch_6_practice_problems/SumOfArray.java
@@ -0,0 +1,10 @@
+
+public class SumOfArray {
+   public static void main(String[] args) {
+      float[] nums = {45, 334.4f, 23.55f, 56.45f, 23.23f};
+      // System.out.println(nums);
+      for (int i = 0; i < nums.length; i++) {
+         System.out.println(nums[i]);
+      }
+   }   
+}
