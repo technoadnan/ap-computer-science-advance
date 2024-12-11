f7ea635016e3ac060ffdc7f8144f24009038d018
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-12-10 23:55:25 -0500

    Add Methods class to Java course for demonstrating the use of methods

---
+++ java-course/30_methods/Methods.java
@@ -0,0 +1,10 @@
+// package java-course.30_methods;
+
+
+// another name of method is called function in other programming language    
+public class Methods {
+   int mySum(int a, int b) {
+      int c = a+b;
+      return c;	//Return value
+   }
+}
