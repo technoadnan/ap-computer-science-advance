f35fd54e91921d7fca49785c334e802ebd326e70
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-12-27 21:56:02 -0500

    Add IntroToOops class to Java course for introducing the concept of object-oriented programming

---
+++ java-course/35_intro_to_oops/IntroToOops.java
@@ -0,0 +1,27 @@
+public class IntroToOops {
+   public static void main(String[] args) {
+
+      // DRY - don't repeat yourself
+      // class - is a blueprint for creating objects 
+      // the blue print 
+
+      // Noun - Class - Employee
+      // Adjective - Attributes - name, age, salary
+      // Verb - Methods - getSalary(), increment()
+
+
+      // Class :
+      // A class is a blueprint for creating objects.
+      // Classes do not consume any space in the memory.
+      // Objects inherit methods and variables from the class.
+      // It is a logical component.
+
+
+      // Objects :
+      // An object is an instantiation of a class. When a class is defined, a template (info) is defined.
+      // Every object has some address, and it occupies some space in the memory.
+      // It is a physical entity.
+
+
+   }
+}
