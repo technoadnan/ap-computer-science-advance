d1cb2a451237261b706e82ea7fe5f517705ad97b
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-12-27 21:56:11 -0500

    Add OopsBasicTerms class to Java course for introducing basic terms of object-oriented programming

---
+++ java-course/36_basic_terms_oops/OopsBasicTerms.java
@@ -0,0 +1,10 @@
+public class OopsBasicTerms {
+   public static void main(String[] args) {
+      // Abstraction, hids internal details
+      // encapsulation, puts lots of things together
+      // Inheritance, deriving new things from existing things || DRY
+      // polymorphism, one entity many forms, smartphone -> phone || smartphone -> calculator
+      
+
+   }   
+}
