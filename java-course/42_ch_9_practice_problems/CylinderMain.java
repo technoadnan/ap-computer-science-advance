b0b9db17e8152facc8bae2be12161f96e083828b
Adnan Bin Alam <code.technoadnan@gmail.com>
2025-01-11 23:47:39 -0500

    Add CalculateSurfaceArea method to Cylinder class in CylinderMain.java

--- java-course/42_ch_9_practice_problems/CylinderMain.java
+++ java-course/42_ch_9_practice_problems/CylinderMain.java
@@ -1,20 +1,29 @@
 
 
 
+
+
 public class CylinderMain {
    public static void main(String[] args) {
       Cylinder cy = new Cylinder();
       cy.setRadius(34);
       cy.setHeight(34);
-
-      
+      int a = cy.CalculateSurfaceArea(2);
+      System.out.println(a);
    }
+
+
 }
 
 
 class Cylinder {
    private int radius;
    private int height;
+
+   public Cylinder(int height, int radius) {
+      this.height = height;
+      this.radius = radius;
+   }
    
    public void setRadius(int n){
       this.radius = n;
@@ -30,6 +39,11 @@ class Cylinder {
 
    public int getHeight(int n) {
       return height;
+   }
+
+
+   public int CalculateSurfaceArea(int radius) {
+      return (int)(4*Math.PI*(Math.pow(radius, 2)));
    }
 
 
