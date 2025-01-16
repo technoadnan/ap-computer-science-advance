73ebe79270275103c805b0a6a715bde071dbb7f6
Adnan Bin Alam <code.technoadnan@gmail.com>
2025-01-15 23:57:43 -0500

    Add ThisSuper class to Java course for demonstrating the usage of the 'this' and 'super' keywords in Java

---
+++ java-course/45_this_super/ThisSuper.java
@@ -0,0 +1,20 @@
+class one {
+   int a;
+   public int getA(){
+      return a;
+   }
+}
+
+// class two extends one{
+
+// }
+
+
+public class ThisSuper {
+   public static void main(String[] args) {
+   // Super keyword 
+   // A reference variable used to refer immediate parent class object.
+   // It can be used to refer immediate parent class instance variable.
+   // It can be used to invoke the parent class method.
+   }
+}
