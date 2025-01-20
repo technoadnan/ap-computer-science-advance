a2080c738fa322858eb9942d00d33a1ad4ba3ba8
Adnan Bin Alam <code.technoadnan@gmail.com>
2025-01-19 23:58:32 -0500

    Add CircleInheritence class to Java course for demonstrating inheritance in Java

---
+++ java-course/49_ch_10_practice_problems/CircleInheritence.java
@@ -0,0 +1,18 @@
+class Circle{
+   public int radius;
+
+   public double area() {
+      return Math.PI*this.radius*this.radius;
+   }
+
+}
+
+class Cylinder extends Circle{
+   
+}
+
+public class CircleInheritence {
+   public static void main(String[] args) {
+      
+   }
+}
